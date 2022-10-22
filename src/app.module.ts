78cfda00c2533975ef3a499dd4d452641de379a3
Danya_Vid <danilviduykov@gmail.com>
2022-10-22 12:50:47 +0500

    add static file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { FileModule } from './file/file.module';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PizzaModule } from './pizza/pizza.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static'),
+    }),
     MongooseModule.forRoot(
       'mongodb+srv://danil:danil@cluster0.djdqiit.mongodb.net/pizza-store?retryWrites=true&w=majority',
     ),
